d8e492cce0ddb89b76e2940137c501fc76a9f7a6
svensvenson <svendalf@gmail.com>
2014-11-07 19:48:02 +0100

    update map ui, display route infos, localize date and text, minor additions

--- _plugins/gpx-blogger.rb
+++ _plugins/gpx-blogger.rb
@@ -14,21 +14,32 @@ module GPXBlogger
                 imageDir = "#{site.source}/images/#{name}/map/"
                 input = "#{site.source}/routes/#{name}.gpx"
                 output = "#{site.source}/routes/#{name}.json"
+                outputinfo = "#{site.source}/routes/#{name}-info.json"
 
                 puts "image dir: #{imageDir}"
                 puts "output: #{output}"
                 puts "input: #{input}"
+                puts "outputinfo: #{outputinfo}"
 
-                post.data['geojson'] = "#{site.baseurl}routes/#{name}.json"
-                post.data['geoimagedir'] = "#{site.baseurl}images/#{name}/map/"
+                post.data['geojson'] = "#{site.baseurl}/routes/#{name}.json"
+                post.data['geoimagedir'] = "#{site.baseurl}/images/#{name}/map/"
                 post.data['mapid'] = name.tr('-','')
-                result = `python #{site.source}/_plugins/geoJsonGenerator/generateGeoJsonFromGPX.py -i #{imageDir} -o #{output} #{input}`
+                
+                result = `python #{site.source}/_plugins/geoJsonGenerator/generateGeoJsonFromGPX.py -i #{imageDir} -o #{output} -s #{outputinfo} #{input}`
                 puts result
+
+                # load route infos from json and add it to post data
+                routeinfo = JSON.parse( IO.read(outputinfo))
+                post.data['uphill'] = routeinfo['uphill']
+                post.data['length_3d'] = routeinfo['length_3d']
+                post.data['length_2d'] = routeinfo['length_2d']
+                post.data['downhill'] = routeinfo['downhill']
+                post.data['imageCount'] = routeinfo['imageCount']
+                post.data['routeinfo'] = routeinfo              
 
                 site.static_files << Jekyll::StaticFile.new(site, site.source, '/routes/', "#{name}.json") #mark generated json-file as changed so it is copied to the site by jekyll
             end
         end
-
     end
   end
 end
--- _posts/2014-06-15-wanderung-seeberg.md
+++ _posts/2014-06-15-wanderung-seeberg.md
@@ -1,7 +1,6 @@
 ---
 layout: map-post
 title: Wanderung auf den Seeberg
-description: "Wanderung auf den Daniel"
 modified: 2014-10-08
 tags: [wanderung]
 comments: false
